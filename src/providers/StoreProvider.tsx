e7b4b7f5b9edd1fc662cb1baf96f0b9091a1ed66
hmmasudsarder <hmmasudsardar@gmail.com>
2025-04-06 18:56:32 +0600

    change and fixit the bug

---
+++ src/providers/StoreProvider.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import Loading from "@/components/ui/loading";
+import { AppStore, makeStore } from "@/redux/store";
+import { ReactNode, useRef } from "react";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+export default function StoreProvider({ children }: { children: ReactNode }) {
+  const storeRef = useRef<AppStore>(undefined);
+
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+
+  const persistedStore = persistStore(storeRef.current);
+
+  return (
+    <Provider store={storeRef.current}>
+      {/* <PersistGate loading={<Loading />} persistor={persistedStore}> */}
+      {children}
+      {/* </PersistGate> */}
+    </Provider>
+  );
+}
